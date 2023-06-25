f76d702f5ce18229a8eedf231fee72990e08a7e4
Zach Asher <zach.asher@hotmail.co.uk>
2023-06-26 00:35:42 +0300

    Finished video details section

--- src/components/VideoDetails/VideoDetails.js
+++ src/components/VideoDetails/VideoDetails.js
@@ -1,3 +1,5 @@
+import "./videodetails.scss";
+
 import viewsIcon from "./../../assets/icons/views.svg";
 import likesIcon from "./../../assets/icons/likes.svg";
 
@@ -6,21 +8,22 @@ function VideoDetails(props) {
     <section className="active-video-details">
       <h1 className="active-video-details__title">{props.video.title}</h1>
       <article className="stats">
-        <div className="stats__1">
-          <p className="stats__1-channel">By {props.video.channel}</p>
-          <p className="stats__1-date">{props.video.timestamp}</p>
+        <div className="stats__left">
+          <p className="stats__channel">By {props.video.channel}</p>
+          <p className="stats__date">{props.video.timestamp}</p>
         </div>
-        <div className="stats__2">
-          <div className="stats__2-views">
-            <img src={viewsIcon} className="stats__2-views-icon"></img>
-            <p className="stats__2-views-count">{props.video.views}</p>
+        <div className="stats__right">
+          <div className="stats__views">
+            <img src={viewsIcon} className="stats__views--icon"></img>
+            <p className="stats__views--count">{props.video.views}</p>
           </div>
-          <div className="stats__2-likes">
-            <img src={likesIcon} className="stats__2-likes-icon"></img>
-            <p className="stats__2-likes-count">{props.video.likes}</p>
+          <div className="stats__likes">
+            <img src={likesIcon} className="stats__likes--icon"></img>
+            <p className="stats__likes--count">{props.video.likes}</p>
           </div>
         </div>
       </article>
+      <p className="active-video-details__description">{props.video.description}</p>
     </section>
   );
 }
